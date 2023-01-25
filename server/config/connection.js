3a247231cae1e7aa19be7c7525098bf1508d2ee7
infinissible <infinissible@gmail.com>
2023-01-25 05:13:35 -0800

    package-lock deleted

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb+srv://Callank21:qPE0mNLU7jh5490B@cluster0.c0mdl.mongodb.net/?retryWrites=true&w=majority',
+  process.env.MONGODB_URI || 'mongodb://localhost/freelancing-gig',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
